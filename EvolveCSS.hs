54251a2fee7d756f65f2a1fb7eb5f2e294700ba3
David Crespo <crespo.dm@gmail.com>
2014-08-19 00:12:45 -0500

    refactor with applicative

--- EvolveCSS.hs
+++ EvolveCSS.hs
@@ -1,3 +1,4 @@
+import Control.Applicative hiding ((<|>))
 import Text.Parsec
 import Text.Parsec.String
 
@@ -5,25 +6,18 @@ type Selector = String
 data Rule = Rule String String deriving Show
 data Ruleset = Ruleset Selector [Rule] deriving Show
 
+paddedChar c = char c <* spaces
+
 rule :: Parser Rule
 rule = do
-    p <- many1 letter
-    char ':'
-    spaces
+  p <- many1 letter <* paddedChar ':'
+  v <- many1 (noneOf ";") <* paddedChar ';'
 
-    v <- many1 letter
-    char ';'
-
-    return $ Rule p v
+  return $ Rule p v
 
 ruleset :: Parser Ruleset
 ruleset = do
     s <- many1 (noneOf "{")
-    char '{'
-    spaces
-
-    r <- many1 rule
-    char '}'
-    spaces
+    r <- paddedChar '{' *> many1 rule <* paddedChar '}'
 
     return $ Ruleset s r
