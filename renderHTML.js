0616d607fd53ea07c1d91d939dedec51edf03a4d
David Crespo <crespo.dm@gmail.com>
2014-08-19 22:04:13 -0500

    use stylesheet instead of inline styles

--- renderHTML.js
+++ renderHTML.js
@@ -1,10 +1,14 @@
+console.log('loading libaries...');
 var page = require('webpage').create(), fs = require('fs');
 
 var fullpath = fs.workingDirectory + fs.separator + 'index.html';
+
+console.log('rendering...');
 
 page.viewportSize = { width: 800, height: 600 };
 
 page.open(fullpath, function start(status) {
   page.render('render/index.png');
+  console.log('done');
   phantom.exit();
 });
