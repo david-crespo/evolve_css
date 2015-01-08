40280c8af2a2da4804c64f39abc273b600efb828
David Crespo <crespo.dm@gmail.com>
2015-01-07 23:46:19 -0600

    addRuleset and generateSelector

--- Mutate.hs
+++ Mutate.hs
@@ -8,5 +8,30 @@ combine :: [a] -> [a] -> [a]
 combine s1 [] = s1
 combine [] s2 = s2
 combine s1 s2 = firstHalf s1 ++ secondHalf s2 where
+    halfLength s = (length s) `div` 2
     firstHalf s = take (halfLength s) s
     secondHalf s = drop (halfLength s) s
+
+
+-- MUTATORS
+
+addRuleset :: Stylesheet -> [Selector] -> Int -> Stylesheet
+addRuleset s sels r = s ++ [newRuleset] where
+    newSelector = eltForSeed sels r
+    newRuleset = Ruleset newSelector []
+
+
+-- GENERATORS
+
+generateSelector :: [Selector] -> Int -> Selector
+generateSelector = eltForSeed
+
+-- we will want to be able to generate more complex selectors, but this is a start
+
+
+-- UTILITIES
+
+-- select an element from a list based on a (large) integer
+eltForSeed :: [a] -> Int -> a
+eltForSeed xs r = xs !! rIdx where
+    rIdx = r `mod` (length xs)
